47fb450441016160caf8c4d78f200f26db842bfb
leandrotf95 <leandrobsb95@gmail.com>
2020-02-24 23:52:59 -0300

    monitorando rotas

--- src/js/modules/cst-app.js
+++ src/js/modules/cst-app.js
@@ -1,4 +1,24 @@
 angular
     .module("cstApp", [
         'ngRoute'
-    ]);
+    ])
+    .run(run);
+
+    run.$inject = ["$rootScope"];
+
+    function run(
+        $rootScope
+    )
+    {
+
+        //Aciona sempre que o usuário trocar de página
+        $rootScope.$on('$routeChangeStart', function (evt, route) {
+            console.log(evt);
+            
+            console.log(route.originalPath);
+            
+            
+            
+        });
+
+    }
